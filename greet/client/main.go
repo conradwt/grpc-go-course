89fcd2c66785fa84263f2ad92161982bbaccb41d
Conrad Taylor <conradwt@gmail.com>
2022-04-16 17:56:25 -0700

    Code cleanup.

--- greet/client/main.go
+++ greet/client/main.go
@@ -9,10 +9,6 @@ import (
 
 var addr string = "localhost:50051"
 
-// type Server struct {
-// 	pb.GreetServiceServer
-// }
-
 func main() {
 	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
