1956d821490e671fb7c628a201babf33f09fed7e
Conrad Taylor <conradwt@gmail.com>
2022-04-16 19:09:07 -0700

    Update naming.

--- greet/server/greet.go
+++ greet/server/greet.go
@@ -7,10 +7,10 @@ import (
 	pb "github.com/conradwt/grpc-go-course/greet/proto"
 )
 
-func (s *Server) Greet(ctx context.Context,in *pb.GreetRequest) (*pb.GreeResponse, error) {
+func (s *Server) Greet(ctx context.Context,in *pb.GreetRequest) (*pb.GreetResponse, error) {
 	log.Printf("Greet function was invoked with %v\n", in)
 
-	return &pb.GreeResponse{
+	return &pb.GreetResponse{
 		Result: "Hello " + in.FirstName,
 	}, nil
 }
