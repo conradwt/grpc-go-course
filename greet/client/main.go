e69c8564c9875fe4019eb3343d3e145a148863ca
Conrad Taylor <conradwt@gmail.com>
2022-06-18 14:00:13 -0700

    Add greet with deadline implementation. (#7)

--- greet/client/main.go
+++ greet/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -24,5 +25,7 @@ func main() {
 	// doGreet(c)
 	// doGreetManyTimes(c)
 	// doLongGreet(c)
-	doGreetEveryone(c)
+	// doGreetEveryone(c)
+	// doGreetWithDeadline(c, 5*time.Second)
+	doGreetWithDeadline(c, 1*time.Second)
 }
